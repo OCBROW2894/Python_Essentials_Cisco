ab636bec6ab663654ee04320140cce5c51d54e5e
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-02-06 12:53:08 +0300

    Updated Payslip

--- Trash code/PaySlip.py
+++ Trash code/PaySlip.py
@@ -10,12 +10,26 @@ eName - Employee Name
 rSalary- Retainer Salary
 gPay- Gross Pay
 nPay- Net Pay
+
+Updates- Changed the Commision Calculations from the general 20% of sales
+        To a Different commision based on amount of sales
 """
 
 eName= input("Please Enter Employees Name: ")
 sales= float(input(f"\n{eName}, Please enter your Sales this month: " ))
 rSalary= 15000.0
-Commision= 0.2 * sales
+#Commision= 0.2 * sales  #Old Commission Formula
+if sales>=1000000:
+    Commision = sales * 0.25
+elif sales>= 500000:
+    Commision = sales * 0.20
+elif sales>= 250000:
+    Commision = sales * 0.1
+elif sales>= 100000:
+    Commision = sales * 0.05
+else:
+    Commision = 0
+    
 gPay= rSalary + Commision
 tax= 0.3 * gPay
 nPay= gPay - tax
