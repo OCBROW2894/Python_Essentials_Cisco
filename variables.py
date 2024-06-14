e21c91c05aca7b833ff67c4ad1c91b4563511630
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-14 13:44:01 +0300

    Variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+
+"""
+	Python doesnt have a multi line comment
+	But U can use the three quotes which acts as a multiline comment
+	
+	Anyway this file is about VARIABLES
+"""
+#Normal declaration
+x=94;
+y=28;
+z=y+x;
+
+#Casting
+a=int(2800);
+b=int(94);
+c=a+b;
+
+print(z);
+print(c);
