15bcf423dce8f46e948af5ab5e3793a184e2f2bf
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-18 14:28:35 +0300

    Legal Variable Nmaes in Python

---
+++ legalNames.py
@@ -0,0 +1,18 @@
+"""
+Legal Variable Names- In Terms of Case Convention
+
+"""
+
+myname="OCBROW";
+my_first_name= "Bryian"; #snake Case
+_my_last_name="Ochieng"; 
+myMiddleName= "Owuor"; #Camel Case
+MYFAVNUMBER= 2894;
+my1stNickName= "Bayo";#Used Number (1)
+
+
+print(my_first_name);
+print(myMiddleName);
+print(_my_last_name);
+print(myname);
+print(MYFAVNUMBER);
