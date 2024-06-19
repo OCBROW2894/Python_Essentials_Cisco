bb66f34683786829fd994cf06c1690c0ced9c6bc
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-19 12:32:06 +0300

    Assigning Multiple Vaues

---
+++ MultipleValues.py
@@ -0,0 +1,23 @@
+"""
+-Still Under Python Variables
+-ASSIGNING MULTIPLE VALUES
+"""
+a,b,c,d = "Orange","Banana","Apple","Grapes"; #Many Values to Multiple Variables
+
+e=f=g="Peach"; #One Value to Multiple Variables
+
+animals =["Dog","Cat","Hawk","Whale","Dolphin"];
+h,i,j,k,l=animals; #Unpack a Collection
+
+print(a);
+print(b);
+print(c);
+print(d);
+print(e);
+print(f);
+print(g);
+print(h);
+print(i);
+print(j);
+print(k);
+print(l);
